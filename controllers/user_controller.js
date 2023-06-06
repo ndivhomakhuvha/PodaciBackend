6de3024eb8481453a8ad7291404faf872d12bcf6
ndivhom <ndibo69@gmail.com>
2023-06-06 16:22:26 +0200

    updated backend

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,27 +1,21 @@
 import bcrypt from "bcrypt";
 import client from "../config/database_configuration.js";
 import transporter from "../config/email_configuration.js";
-import 'dotenv/config.js'
-
+import "dotenv/config.js";
 
 async function EmailExists(email) {
   try {
     const emailQuery = {
-      text: 'SELECT * FROM users WHERE email = $1',
+      text: "SELECT * FROM users WHERE email = $1",
       values: [email],
     };
     const emailResult = await client.query(emailQuery);
-    return (
-      (emailResult && emailResult.rows.length > 0)
-    );
-  }
-  catch (error) {
-    console.error('Error checking user existence:', error);
+    return emailResult && emailResult.rows.length > 0;
+  } catch (error) {
+    console.error("Error checking user existence:", error);
     throw error;
   }
-
 }
-
 
 const createUser = async (request, response) => {
   const { username, email, password } = request.body;
@@ -29,11 +23,10 @@ const createUser = async (request, response) => {
   const salt = bcrypt.genSaltSync(saltRounds);
   const hashedPassword = bcrypt.hashSync(password, salt);
 
-
   try {
     const exists = await EmailExists(email);
     if (exists) {
-     return response.status(409).send({ message: 'User already exists' })
+      return response.status(409).send({ message: "User already exists" });
     }
     await client.query(
       "INSERT INTO users (username, email,password) VALUES ($1, $2, $3) RETURNING *",
@@ -44,14 +37,13 @@ const createUser = async (request, response) => {
         }
         return response.status(201).json(results.rows);
       }
-    )
+    );
   } catch (error) {
-    console.error('Error saving user to the database:', error);
+    console.error("Error saving user to the database:", error);
     throw error;
-
   }
-
 };
+
 
 
 
@@ -70,7 +62,6 @@ const Signin = (request, response) => {
       let isPasswordValid = bcrypt.compareSync(password, userPassword);
       let number = Math.floor(1000 + Math.random() * 9000);
       if (isPasswordValid) {
-
         const mailConfigurations = {
           from: process.env.NODEMAILER_USER,
           to: email,
@@ -85,30 +76,57 @@ const Signin = (request, response) => {
         // Send the mail upon everything above correct
         transporter.sendMail(mailConfigurations, function (error, info) {
           if (error) throw Error(error);
-          console.log(info)
+          console.log(info);
         });
         let successObject = {
           email: email,
           username: results.rows[0].username,
-          number: number
-        }
+          number: number,
+        };
 
-        response.status(200).send(successObject)
+        response.status(200).send(successObject);
+      } else {
+        response.status(409).send({ message: "Failure response" });
       }
-      else {
-        response.status(409).send({ message: 'Failure response' })
-      }
-
     }
   );
 };
 
 
-export default { createUser, Signin };
 
+const resendOTP = async (request, response) => {
+  const { email } = request.body;
+  try {
+    const exists = await EmailExists(email);
+    if (!exists) {
+      return response.status(409).send({ message: "User does not exist" });
+    }
+    let number = Math.floor(1000 + Math.random() * 9000);
+    const mailConfigurations = {
+      from: process.env.NODEMAILER_USER,
+      to: email,
+      subject: "Sign in OTP",
+      // This would be the text of email body
+      html:
+        "<h1>Your Sign in OTP:</h1><br/>" +
+        email +
+        `<p>Your OTP is: <strong>${number}</strong><br/><br/>
+                       Made with ❤️ By FlexBox Inc.</p>`,
+    };
+    // Send the mail upon everything above correct
+    transporter.sendMail(mailConfigurations, function (error, info) {
+      if (error) throw Error(error);
+      console.log(info);
+    });
+    let successObject = {
+      email: email,
+      number: number,
+    };
+    response.status(200).send(successObject);
+  } catch (error) {
+    console.error("Error resending OTP", error);
+    throw error;
+  }
+};
 
-
-
-
-
-
+export default { createUser, Signin, resendOTP };
--- routes/user_router.js
+++ routes/user_router.js
@@ -1,8 +1,9 @@
-import database from '../controllers/user_controller.js'
-import express from "express"
+import database from "../controllers/user_controller.js";
+import express from "express";
 const router = express.Router();
 
-router.post('/',database.createUser);
-router.post('/sign', database.Signin);
+router.post("/", database.createUser);
+router.post("/sign", database.Signin);
+router.post("/resend", database.resendOTP);
 
 export default router;
