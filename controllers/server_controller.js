02036de5cc2bbb0fff33f7c0f0ffa7ee2b1ca978
Ndivho69 <ndibo69@gmail.com>
2023-06-12 21:16:49 +0200

    changed

--- controllers/server_controller.js
+++ controllers/server_controller.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import client from "../config/database_configuration.js";
 import { response } from "express";
+import shortUrl from "node-url-shortener";
+
+
+
 
 async function checkUrl(url) {
   let status;
@@ -36,6 +40,11 @@ async function ServerExists(ipadress) {
 const createServer = async (request, response) => {
   let status;
   const { imageurl, ipadress, name, memory, type, user_id } = request.body;
+  shortUrl.short(imageurl, function (err, url) {
+    imageurl = url
+    console.log(imageurl)
+  });
+
 
   checkUrl(`http://${ipadress}`)
     .then((data) => {
