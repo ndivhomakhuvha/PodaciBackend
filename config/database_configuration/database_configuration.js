ec7890af3febad40799d07d7965722ecabceff4c
Ndivho <ndibo69@gmail.com>
2024-02-01 14:52:08 +0200

    SSL Certificate for security

--- config/database_configuration/database_configuration.js
+++ config/database_configuration/database_configuration.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
-import Pool from 'pg'
+import Pool from "pg";
+import fs from 'fs/promises';
 
-
+const sslCertPath = "ssl/prod-ca-2021.crt";
 
 
 const PoolConstructor = Pool.Pool;
@@ -11,12 +12,13 @@ const pool = new PoolConstructor({
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASS,
   port: process.env.PORT_DB,
-  ssl: false,
+  ssl: {
+    ca: await fs.readFile(sslCertPath, 'utf-8'),
+  },
   synchronize: true,
   extra: {
     trustServerCertificate: true,
-  }
-
+  },
 });
 
-export default pool
+export default pool;
